6826de1f02cc1bf51856cc5a55ba17390af37659
Jane <jane.edwards.mcnear@gmail.com>
2018-10-29 14:22:12 -0700

    added time and space complexity

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,22 @@
 # Creates a new array to return the intersection of the two input arrays
+# Time complexity: O(n x m)
+# Space complexity: O(n)
+
 def intersection(array1, array2)
-  raise NotImplementedError
+  intersection_arr = []
+  return intersection_arr if array1.nil? || array2.nil?
+
+  i = 0
+  while array1.length > i
+    j = 0
+    while array2.length > j
+      if array1[i] == array2[j]
+        intersection_arr << array1[i]
+      end
+      j += 1
+    end
+    i += 1
+  end
+
+  return intersection_arr
 end
